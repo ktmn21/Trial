b787f9adbb9cd82b4ca122d0f2d4391012c1ad89
Kutman <kutman2002mukarapov@gmail.com>
2024-10-24 23:18:01 +0600

    Trial with remote

---
+++ src/main/java/org/example/tash/TashApplication.java
@@ -0,0 +1,13 @@
+package org.example.tash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TashApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tash/TashApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
